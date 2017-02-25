c9d211964631f30d17e0452bbf8d3c9776bbf95b
ysevin <ysevin@163.com>
2017-02-25 17:06:38 +0800

    m

---
+++ dev/sql/personnel.sql
@@ -0,0 +1,16 @@
+create table persons (
+    id bigint primary key auto_increment,
+    nationality varchar(255) default "",
+    area varchar(255) default "",
+    birthday date not null,
+    gender varchar(255) not null, 
+    entry date not null, 
+	company varchar(255) default "",
+	address varchar(255) default "",
+	phone bigint not null,
+	fingerprint BLOB,
+	photo BLOB,
+	remark varchar(255), default "",
+);
+
+insert into persons(id, nationality, area, birthday, gender, entry, company, address, photo, fingerprint, photo, remark) value(1, "非洲", "1990-01-01", "男", "2010-01-01", "网易", "天河", 1380000000, null, null, "123");
