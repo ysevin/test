de57a0314c6b1b30a96c3be41046bf61e968bdb4
ysevin <ysevin@163.com>
2017-02-25 17:44:18 +0800

    m

--- dev/sql/personnel.sql
+++ dev/sql/personnel.sql
@@ -13,4 +13,4 @@ create table persons (
 	remark varchar(255), default "",
 );
 
-insert into persons(id, nationality, area, birthday, gender, entry, company, address, photo, fingerprint, photo, remark) value(1, "非洲", "1990-01-01", "男", "2010-01-01", "网易", "天河", 1380000000, null, null, "123");
+insert into persons(id, nationality, area, birthday, gender, entry, company, address, photo, fingerprint, photo, remark) value(1, "非洲", "刚果", "1990-01-01", "男", "2010-01-01", "网易", "天河", 1380000000, null, null, "123");
