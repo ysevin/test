617415b69cc86bd7b4708b90a119d30cb741b550
ysevin <ysevin@163.com>
2017-03-10 21:05:03 +0800

    m

--- dev/nginx/html/common.js
+++ dev/nginx/html/common.js
@@ -13,6 +13,93 @@ var image_controls = {
 	photo:"照片",
 	fingerprint:"指纹"};
 var structure_num = 1;
+var voice_content = "xxx"
+
+var base64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+var base64DecodeChars = new Array(
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63,
+        52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1,
+        -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+        15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1,
+        -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+        41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1);
+function base64encode(str) {
+    var out, i, len;
+    var c1, c2, c3;
+    len = str.length;
+    i = 0;
+    out = "";
+    while(i < len) {
+        c1 = str.charCodeAt(i++) & 0xff;
+        if(i == len)
+        {
+            out += base64EncodeChars.charAt(c1 >> 2);
+            out += base64EncodeChars.charAt((c1 & 0x3) << 4);
+            out += "==";
+            break;
+        }
+        c2 = str.charCodeAt(i++);
+        if(i == len)
+        {
+            out += base64EncodeChars.charAt(c1 >> 2);
+            out += base64EncodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
+            out += base64EncodeChars.charAt((c2 & 0xF) << 2);
+            out += "=";
+            break;
+        }
+        c3 = str.charCodeAt(i++);
+        out += base64EncodeChars.charAt(c1 >> 2);
+        out += base64EncodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
+        out += base64EncodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >>6));
+        out += base64EncodeChars.charAt(c3 & 0x3F);
+    }
+    return out;
+}
+function base64decode(str) {
+    var c1, c2, c3, c4;
+    var i, len, out;
+    len = str.length;
+    i = 0;
+    out = "";
+    while(i < len) {
+        /* c1 */
+        do {
+            c1 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+        } while(i < len && c1 == -1);
+        if(c1 == -1)
+            break;
+        /* c2 */
+        do {
+            c2 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+        } while(i < len && c2 == -1);
+        if(c2 == -1)
+            break;
+        out += String.fromCharCode((c1 << 2) | ((c2 & 0x30) >> 4));
+        /* c3 */
+        do {
+            c3 = str.charCodeAt(i++) & 0xff;
+            if(c3 == 61)
+                return out;
+            c3 = base64DecodeChars[c3];
+        } while(i < len && c3 == -1);
+        if(c3 == -1)
+            break;
+        out += String.fromCharCode(((c2 & 0XF) << 4) | ((c3 & 0x3C) >> 2));
+        /* c4 */
+        do {
+            c4 = str.charCodeAt(i++) & 0xff;
+            if(c4 == 61)
+                return out;
+            c4 = base64DecodeChars[c4];
+        } while(i < len && c4 == -1);
+        if(c4 == -1)
+            break;
+        out += String.fromCharCode(((c3 & 0x03) << 6) | c4);
+    }
+    return out;
+}
 
 function connect() 
 {
@@ -202,9 +289,34 @@ function log(text)
 	document.getElementById('log').appendChild(li)
 	return false;
 }
+function test_voice()
+{
+	if (websocket_channel === null) 
+		return log('please connect first');
+	var str = '{ "websocket_cmd":"upload_voice",'
+
+	str += '"' + "file" + '":"' + voice_content + '",'
+
+	str = str.substring(0, str.length-1)
+	str += "}"
+	log('upload: ' + websocket_channel_addr + ", str: " + str);
+	websocket_channel.send(str);
+	window.event.returnValue=false;  
+}
+
+function read_voice_file(file_id)
+{
+	var files = document.getElementById(file_id).files
+	var file = files[0];
+    var reader = new FileReader();
+    reader.readAsBinaryString(file);
+    reader.onload = function() {
+		voice_content = base64encode(this.result)
+		log('voice: ' +  voice_content);
+    }
+}
 function read_file(file_id, img_id)
 {
-	console.log("===", file_id, img_id)
 	var files = document.getElementById(file_id).files
 	var file = files[0];
 	var maxsize = 64 * 1024;
@@ -304,6 +416,14 @@ function create_my_element(str)
 		input.onchange =  function() {read_file(input.id, strs[1])}
 		ele = input
 	}
+	else if(str[0] == "v")
+	{
+		var input = document.createElement("input")
+		input.type = "file"
+		input.id = strs[1] + "_file"
+		input.onchange =  function() {read_voice_file(input.id)}
+		ele = input
+	}
 	else
 	{
 		ele = document.createElement("label")
@@ -395,9 +515,11 @@ function create_button()
 function create_login_form(parent_id)
 {
 	var text_ar = [
-		["l,用户名", "i,user_name"],
-		["l,密码", "i,user_password"],
-		["b,登录,login"],
+		//["l,用户名", "i,user_name"],
+		//["l,密码", "i,user_password"],
+		//["b,登录,login"],
+		["b,上传,test_voice"],
+		["v,打开,read_voice_file"],
 	]
 	var fo = document.getElementById(parent_id)
 	var lfo = document.createElement("form")
