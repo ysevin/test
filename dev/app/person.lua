8cf5eb008a38bf2b14e0cc732f19b84572dda1a2
ysevin <ysevin@163.com>
2017-03-12 21:21:28 +0800

    m

--- dev/app/person.lua
+++ dev/app/person.lua
@@ -2,11 +2,12 @@ local cjson = require "cjson"
 local http = require "resty.http"
 local mysql_client = require "mysql_client"
 local cache_client = require "cache_client"
+require "util"
 
 local person_handler = { }
 person_handler._VERSION = '1.0'
 
-person_handler.baidu_voice_token = person_handler.baidu_voice_token or ""
+person_handler.baidu_voice_token = ""
 
 function person_handler.send_data(_peer_ctx, _data)
     local send_str = cjson.encode(_data)
@@ -119,6 +120,20 @@ function person_handler.upload_voice(_peer_ctx, _msg)
 end
 
 function person_handler.down_voice(_peer_ctx, _msg)
+	person_handler.search_info(_peer_ctx, _msg["text"])
+	--print("=====", utf8len("我223爱中华人民共和图"))
+
+	--[[
+	local insert_dict = {word="我想听刻舟求剑", key_word = "刻舟求剑",search_num = 0, weight = 0}
+    --local ret = mysql_client:insert_not_check("tory_info_index", insert_dict)
+	local query_dict = {word="我想听刻舟求剑xxxx"}
+    local ret = mysql_client:read_condition_not_check("tory_info_index", query_dict)
+	--local ret = cjson.encode(ret)
+	ss(ret)
+	--]]
+
+
+	--[[
 	local httpc = http.new()
 	local client_id="Hbk9mhQpnDtCfNCBx82DZvh4"
 	local client_secret= "cac9e8e002b4d8212426be3b511e5ee6"
@@ -138,22 +153,6 @@ function person_handler.down_voice(_peer_ctx, _msg)
 	local url = string.format("http://tsn.baidu.com/text2audio?tex=%s&lan=zh&cuid=00:0c:29:5c:c9:56&ctp=1&tok=%s", baike_info, person_handler.baidu_voice_token)
 	print(url)
 
-	--[[
-	local request_body = string.format('{"tex":"%s","lan":"zh","tok":"%s","cuid":"%s","ctp":2}',
-	_msg["text"], person_handler.baidu_voice_token, "00:0c:29:5c:c9:56")
-	print(request_body)
-
-    local httpc = http.new()
-	local res, err = httpc:request_uri("http://tsn.baidu.com/text2audio",{
-		method = "POST",
-		headers = {
-			["Content-Type"] = "application/json; charset=utf-8";
-			["Content-Length"] = #request_body;
-			},
-		body = request_body, --需要用json格式
-	})
-	print(res.body)
-	--]]
     local ack_down_dict = { }
     ack_down_dict.user_result = -1
     ack_down_dict.user_error = ""
@@ -164,69 +163,124 @@ function person_handler.down_voice(_peer_ctx, _msg)
 
     person_handler.send_data(_peer_ctx, ack_down_dict)
     return true
+	--]]
 end
 
-function person_handler.get_baike_info(_text)
-	local url = string.format("http://baike.baidu.com/api/openapi/BaikeLemmaCardApi?scope=103&format=json&appid=379020&bk_key=%s&bk_length=600", _text)
-	local httpc = http.new()
-	local res, err = httpc:request_uri(url,{
-		ssl_verify = false,
-		})
-	if res.status ~= ngx.HTTP_OK then 
-		return
+function person_handler.get_baidu_token()
+	person_handler.baidu_voice_token = nil
+	if not person_handler.baidu_voice_token then
+		local httpc = http.new()
+		local client_id="Hbk9mhQpnDtCfNCBx82DZvh4"
+		local client_secret= "cac9e8e002b4d8212426be3b511e5ee6"
+		local url = string.format("https://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials&client_id=%s&client_secret=%s", client_id, client_secret)
+		local res, err = httpc:request_uri(url,{
+			ssl_verify = false,		--https的要写这个
+			})
+		if res.status == ngx.HTTP_OK then 
+			local body_data = cjson.decode(res.body)
+			if body_data then 
+				person_handler.baidu_voice_token = body_data.access_token
+			end
+		end
 	end
-	local body_data = cjson.decode(res.body)
-	if not body_data then 
-		return false 
-	end
-	return body_data.abstract or "无返回"
 end
 
-function person_hander.search_info(_word)
+function person_handler.get_baike_info(_word)
+	person_handler.get_baidu_token()
+	local len = utf8len(_word)
+	for i=1, len do
+		local word = utf8sub(_word, i, len)
+		local url = string.format("http://baike.baidu.com/api/openapi/BaikeLemmaCardApi?scope=103&format=json&appid=379020&bk_key=%s&bk_length=600", word)
+		local httpc = http.new()
+		local res, err = httpc:request_uri(url,{
+			ssl_verify = false,
+			})
+		if res.status ~= ngx.HTTP_OK then 
+			return nil
+		end
+		local body_data = cjson.decode(res.body)
+		if not body_data then 
+			return nil
+		end
+		if body_data.abstract then
+			return word, body_data.abstract
+		end
+	end
+	return nil
+end
+
+function person_handler.get_key_word(_word)
+	local len = utf8len(_word)
+	--先逐字在index库里找
+	for i=1, len do
+		local word = utf8sub(_word, i, len)
+		local query_dict = {word = word}
+    	local ret = mysql_client:read_condition_not_check("tory_info_index", query_dict)
+		if ret and ret[1] then
+			local find = ret[1]
+			return find.key_word
+		end
+	end
+	--再在info库里找
+	for i=1, len do
+		local word = utf8sub(_word, i, len)
+		local query_dict = {key_word = word}
+    	local ret = mysql_client:read_condition_not_check("tory_info", query_dict)
+		if ret and ret[1] then
+			local find = ret[1]
+			--插入到index库
+			local insert_dict = {word = _word, key_work = word}
+			ret = mysql_client:insert_not_check("tory_info_index", insert_dict)
+
+			return word
+		end
+	end
+	return nil
+end
+
+function person_handler.search_info(_peer_ctx, _word)
 	--1, 先从库里找
-	local query_dict = {key_word = _word}
-    local ret = mysql_client:read_condition_not_check("key_word_index", _msg.query_dict)
-	local search_num = 0
-	if ret then
-		search_num = res.search_num
-	end
+	local key_word = person_handler.get_key_word(_word)
 
 	local info = {}
-	if not ret then
+	if not key_word then
 		--2, 如果没条目, 查百科
-		local text_info = person_hander.get_baike_info(_word)
+		local key_word, text_info = person_handler.get_baike_info(_word)
 		if text_info then
-			local cate = get_categroy(text_info)
-		else
-			--百科没有的, 归类到日常, 需要入工输入信息
-			local cate = "日常"
+			--3, 入库
+			local info_insert_dict = {key_word=key_word, info = text_info, type = is_voice and "voice" or "text"}	--后期加入语音数据
+			ret = mysql_client:insert_not_check("tory_info", info_insert_dict, {info="MediumBlob"})
+
+			info = {info = text_info, type = "text"}
 		end
-
-		--3, 入库
-		local index_insert_dict = {key_word=_word, categroy = cate, search_num = search_num, weighti = 0}
-		ret = mysql_client:insert_not_check("key_word_index", index_insert_dict)
-
-		local info_insert_dict = {key_word=_word, info = text_info, type = is_voice and "voice" or "text"}	--后期加入语音数据
-		ret = mysql_client:insert_not_check("key_word_info", info_insert_dict, {info="MediumBlob"})
-
-		info = {info = text_info, type = "text"}
+		local insert_dict = {word = _word, key_word=key_word, categroy = "百科", search_num = search_num, weight = 0}
+		local ret = mysql_client:insert_not_check("tory_info_index", insert_dict)
 	else
-    	local ret = mysql_client:read_condition_not_check("key_word_info", _msg.query_dict)
-		info = ret
+		local query_dict = {key_word = key_word}
+    	local ret = mysql_client:read_condition_not_check("tory_info", query_dict)
+		if ret and ret[1] then
+			info = ret[1]
+		end
 	end
 
 	--对该词的搜索次数加1
-	local insert_data = {search_num = search_num+1}
-    ret = mysql_client:insert_not_check("key_word_index", insert_data, nil, query_dict)
+	local query_dict = {word = _word}
+    local ret = mysql_client:read_condition_not_check("tory_info_index", query_dict)
+	if ret and ret[1] then
+		local find = ret[1]
+		local insert_data = {search_num = find.search_num+1}
+		ret = mysql_client:insert_not_check("tory_info_index", insert_data, nil, query_dict)
+	end
 
 	-- 终端输出
 	local ret_info = {}
 	if info.type == "text" then
 		--返回语音合成url
-		ret_info = string.format("http://tsn.baidu.com/text2audio?tex=%s&lan=zh&cuid=00:0c:29:5c:c9:56&ctp=1&tok=%s", info.info, person_handler.baidu_voice_token)
+		person_handler.get_baidu_token()
+		ret_info.voice_info_list = string.format("http://tsn.baidu.com/text2audio?tex=%s&lan=zh&cuid=00:0c:29:5c:c9:56&ctp=1&tok=%s", info.info, person_handler.baidu_voice_token)
 	else
 		--直接返回语音数据
-		ret_info = info.info
+		ret_info.voice_info_list = info.info
 	end
     person_handler.send_data(_peer_ctx, ret_info)
 end
