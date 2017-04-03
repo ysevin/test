5bd5e655f05c58169d7cadb195ba9cd2c92781c1
ysevin <ysevin@163.com>
2017-04-03 21:05:43 +0800

    m

--- dev/nginx/html/toy_info.js
+++ dev/nginx/html/toy_info.js
@@ -4,19 +4,6 @@ function toy_info_del(id)
 	var sd = new Array
 	sd["id"] = id
 	send_data("del_toy_info", sd)
-	/*
-	if (websocket_channel === null) 
-		return log('please connect first');
-	var str = '{ "websocket_cmd":"del_toy_info",'
-
-	str += '"id":' + id + ','
-
-	str = str.substring(0, str.length-1)
-	str += "}"
-	log('del_toy_info: ' + websocket_channel_addr + ", str: " + str);
-
-	websocket_channel.send(str);
-	*/
 
 	window.event.returnValue=false;  
 }
@@ -29,24 +16,6 @@ function toy_info_update_text(arg)
 	sd["key_word"] = ar[1]
 	sd["info"] = text.value
 	send_data("update_toy_info", sd)
-
-	/*
-	if (websocket_channel === null) 
-		return log('please connect first');
-	var str = '{ "websocket_cmd":"update_toy_info",'
-
-	var ar = arg.split("|");
-	var text = document.getElementById("text_" + ar[0]);
-	str += '"key_word":"' + ar[1] + '",'
-	str += '"info":"' + text.value + '",'
-
-
-	str = str.substring(0, str.length-1)
-	str += "}"
-	log('update_toy_info: ' + websocket_channel_addr + ", str: " + str);
-
-	websocket_channel.send(str);
-	*/
 
 	window.event.returnValue=false;  
 }
@@ -66,24 +35,6 @@ function toy_info_add_text()
 	sd["key_word"] = word.value
 	sd["info"] = info.value
 	send_data("add_toy_info", sd)
-
-	/*
-	if (websocket_channel === null) 
-		return log('please connect first');
-	var str = '{ "websocket_cmd":"add_toy_info",'
-
-	var word = document.getElementById("text_word");
-	var info = document.getElementById("text_info");
-	str += '"key_word":"' + word.value + '",'
-	str += '"info":"' + info.value + '",'
-	str += '"type":"text",'
-
-	str = str.substring(0, str.length-1)
-	str += "}"
-	log('add_toy_info: ' + websocket_channel_addr + ", str: " + str);
-
-	websocket_channel.send(str);
-	*/
 
 	window.event.returnValue=false;  
 }
@@ -108,20 +59,6 @@ function toy_info_search()
 	sd["key_word"] = word.value
 	send_data("search_toy_info", sd)
 
-	/*
-	if (websocket_channel === null) 
-		return log('please connect first');
-	var str = '{ "websocket_cmd":"search_toy_info",'
-
-	var word = document.getElementById("search_text");
-	str += '"key_word":"' + word.value + '",'
-
-	str = str.substring(0, str.length-1)
-	str += "}"
-	log('search_toy_info: ' + websocket_channel_addr + ", str: " + str);
-
-	websocket_channel.send(str);
-	*/
 	window.event.returnValue=false;  
 }
 
@@ -131,7 +68,6 @@ function toy_info_form_recv(str)
 	if(obj.toy_info_update_ret)
 	{
 		var info = obj.toy_info_update_ret
-		var au = document.getElementById("audio_" + info.id)
 	}
 	else if(obj.toy_info_list)
 	{
@@ -140,20 +76,20 @@ function toy_info_form_recv(str)
 		{
 			var ar = new Array()
 			var info = obj.toy_info_list[idx]
-			ar[0] = "l," + info["key_word"]
-			if(info["type"] == "voice")
+			ar[0] = "l," + info.key_word
+			if(info.type == "voice")
 			{
-				ar[1] = 'v,audio_'+ info["id"] + ',' + info["info"]
-				ar[2] = "b,É¾³ý,toy_info_del(" + info["id"] + ")"
-				var arg = 'voice_file_' + info["id"] + "|" + info["key_word"]
+				ar[1] = 'v,audio_'+ info.id + ',' + info.info
+				ar[2] = "b,É¾³ý,toy_info_del(" + info.id + ")"
+				var arg = 'voice_file_' + info.id + "|" + info.key_word
 				ar[3] = "b,¸üÐÂ,toy_info_update_music('" + arg + "')"
-				ar[4] = "vf,voice_file_" + info["id"]
+				ar[4] = "vf,voice_file_" + info.id
 			}
 			else
 			{
-				ar[1] = "i,text_"+ info["id"] + "," + info["info"]
-				ar[2] = "b,É¾³ý,toy_info_del(" + info["id"] + ")"
-				var arg = info["id"] + "|" + info["key_word"]
+				ar[1] = "i,text_"+ info.id + "," + info.info
+				ar[2] = "b,É¾³ý,toy_info_del(" + info.id + ")"
+				var arg = info.id + "|" + info.key_word
 				ar[3] = "b,¸üÐÂ,toy_info_update_text('" + arg + "')"
 			}
 			tb_ar[idx] = ar
@@ -163,7 +99,7 @@ function toy_info_form_recv(str)
 			di.parentNode.removeChild(di)
 
 		var lfo = document.getElementById("toy_info_form")
-		di = document.createElement("div_search")
+		di = document.createElement("div")
 		di.id = "div_search"
 		lfo.appendChild(di)
 		create_table_control(di.id, null, tb_ar)
